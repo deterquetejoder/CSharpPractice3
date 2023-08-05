57e96908c3f1747bd471fea579564b923c70193f
deterquetejoder <saecularisator@gmail.com>
2023-08-06 01:19:07 +0500

    ts23 works

--- ts23/Program.cs
+++ ts23/Program.cs
@@ -1,2 +1,10 @@
 ﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
+Console.WriteLine("Enter your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for(int i=1;i<=num;i++)
+{
+    int cubed = i*i*i;
+    Console.WriteLine(cubed);
+}
